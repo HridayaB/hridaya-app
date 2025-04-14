33bbaed8fd245c20b0ea9fc3e913c85e0c1a5e86
HridayaB <hridayab@boise.cs.colostate.edu>
2025-04-13 18:25:45 -0600

    Fixed some minor errors

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { homeConfig } from "./home.config";  
 import { RouterModule } from '@angular/router';
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'app-home',
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
--- src/home/home.config.ts
+++ src/home/home.config.ts
@@ -1,5 +1,25 @@
 export const homeConfig = {
     companyName: 'RGBrigade-29',
     tagline: 'Professional color coordination tools for designers',
-    companyLogo: 'https://cs.colostate.edu:4444/~hridayab/group_project_logo.png'
+    companyLogo: 'https://cs.colostate.edu:4444/~hridayab/group_project_logo.png',
+    contrastLinks: [
+        {
+          textColor: '#003049',
+          bgColor: '#D6C7A2',
+          ratio: '8.26',
+          url: 'https://webaim.org/resources/contrastchecker/?fcolor=003049&bcolor=D6C7A2'
+        },
+        {
+          textColor: '#00008B',
+          bgColor: '#D6C7A2',
+          ratio: '9.14',
+          url: 'https://webaim.org/resources/contrastchecker/?fcolor=00008B&bcolor=D6C7A2'
+        },
+        {
+          textColor: '#103C39',
+          bgColor: '#D6C7A2',
+          ratio: '7.26',
+          url: 'https://webaim.org/resources/contrastchecker/?fcolor=103C39&bcolor=D6C7A2'
+        },
+    ]
 };
